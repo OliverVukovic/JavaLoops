432b74c62b07d388a28590220e2610c4738d5267
Oliver <oliver_vukovic@yahoo.com>
2021-03-03 20:22:26 +0100

    add new Java code

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,26 +5,175 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Loops");
+    //    System.out.println("Loops");
         
-        int age = 22;
+    //    int age = 22;
         
-        if (age > 18) {
-            System.out.println("Osoba je punoletna");
-        } else {
-            System.out.println("Osoba je maloletna");
-        }
-        System.out.println("Koliko imate godina?");
+    //    if (age > 18) {
+    //        System.out.println("Osoba je punoletna");
+    //    } else {
+    //        System.out.println("Osoba je maloletna");
+    //    }
+    //    System.out.println("Koliko imate godina?");
                 
-        Scanner in = new Scanner(System.in);
-        int age2 = in.nextInt();
+    //    Scanner in = new Scanner(System.in);
+    //   int age2 = in.nextInt();
                
-        System.out.println("Uneli ste broj: " + age2);
+    //    System.out.println("Uneli ste broj: " + age2);
         
         // 0-2 beba
         // 3-10 dete
         // 11-17 tinejdzer
         // 18+ odrasla osoba
+    //   if (age >=0 && age <= 2) {
+    //        System.out.println("Osoba je beba");
+    //    } else if (age >= 3 && age <= 11) {
+    //        System.out.println("Osoba je dete");
+    //    } else if (age >= 12 && age <= 17) {
+    //        System.out.println("Osoba je tinejdzer");
+    //    } else if (age >= 18 && age <= 150) {
+    //        System.out.println("Osoba je odrasla");
+    //    } else {
+     //       System.out.println("Uneli ste pogresnu vrednost!");
+    //    }
+        
+     //   System.out.println(".............");
+        
+    //    System.out.println("Unesite redni broj dana u nedelji (1-7): ");
+    //    Scanner in2 = new Scanner(System.in);
+    //    int dayNumber = in2.nextInt();
+        
+     //   System.out.println("Uneli ste broj: " + dayNumber);
+        
+    //   String day = "";
+       
+     //   switch (dayNumber) {
+      //      case 1:
+      //          day = "Ponedeljak";
+      //          break;
+       //     case 2:
+       //         day = "Utorak";
+       //         break;
+      //      case 3:
+      //          day = "Sreda";
+     //           break;
+     //       case 4:
+      //          day = "Cetvrtak";
+      //          break;
+     //       case 5:
+      //          day = "Petak";
+     //           break;
+     //       case 6:
+     //           day = "Subota";
+     //           break;
+     //       case 7:
+     //           day = "Nedelja";
+      //          break;
+    //        default:
+      //          day = "Nepoznat";
+     //           break;
+    //    }
+        
+    //    System.out.println("Dan je: " + day);
+        
+        
+    //    System.out.println("While loop exemple");
+        
+     //   int x = 5;
+    //    while (x < 10) {
+    //        System.out.println(x);
+         //   x = x + 1;
+    //        x++;
+    //    }
+        
+    //    while (x > 0) {
+    //        System.out.println(x);
+    //        x--;
+    //    }
+        
+     //   System.out.println("Trenutna vrednost broja X je: " + x);
+        
+      //  while (x > 5) {
+      //      System.out.println(x);
+      //      x--;
+    //    }
+        
+     //   System.out.println("Do while loop example");
+        
+      //  int y = 45;
+     //   do {
+     //       System.out.println(y);
+     //       y = y + 2;
+     //   } while (y < 40);    
+        
+        
+        
+        System.out.println("For loop example");
+        
+     //   for (int i = 0; i < 10; i++) {
+      //      System.out.println("*");
+      //  }
+        
+     //   for (int i = 99; i >= 90; i++) {
+     //       System.out.println("*");
+    //   }
+        
+    //     for (int i = 0; i < 10; i++) {
+    //          System.out.println("**********");
+    //    }
+          
+      //    for (int i = 1; i <= 10; i++) {
+      //        System.out.println(i + ",1 " + i + ",2 " + i + ",3 " + i + ",4 " + i + ",5 " + i + ",6 " + i + ",7 " + i + ",8 " + i + ",9 ");
+     //   }
+          
+     //     for (int i = 1; i <= 10; i++) {
+     //         for (int j = 1; j <= 10; j++) {
+     //            if (j < 10) {
+      //                System.out.println(i + "," + j + " ");
+      //            } else {  
+     //                 System.out.println(i + "," + j);
+       //           }
+     //         }
+              
+              
+        //      for (int i = 1; i <= 30; i++) {
+        //    for (int j = 1; j <= 30; j++) {
+        //        System.out.print(" * ");
+        //    }
+        //    System.out.println();
+     //   }
+              
+              
+              
+              
+             for (int i = 1; i <= 10; i++) {
+                 for (int j = 1; j <= i; j++) {
+                     System.out.println(" * ");    
+                 }
+                 System.out.println();
+             } 
+        }
+            
+              
+              
+              
+              
+              
+              
+              
     }
     
-}
+        // (0,0) (0,1) (0,2) ..... (0,9)
+        // (1,0) (1,1) ....
+        // ...
+        // ...
+        // (9,0) (9,1) (9,2) ..... (9,9)
+          
+        
+        
+          
+          
+        
+    
+    
+
